708aa670ce1cefbf7caf8cb1fc161d6d7292a7e1
ladosamushia <colkhis@gmail.com>
2019-04-08 10:14:45 -0400

    Some stylistic changes suggested by pyCharm editor.

--- populateHOD.py
+++ populateHOD.py
@@ -1,80 +1,85 @@
-'''
+"""
 Populate a halo catalogue with galaxies using HOD parameters.
 
-For now I will only use the simple 5 parameter HOD. 
-'''
+For now I will only use the simple 5 parameter HOD.
+"""
 
 import scipy.special
 import scipy.optimize
 import numpy as np
-import time
+# import time
 
-def populate(Mhalos, HODpar):
-    '''
-    Mhalos is a numpy array with halo masses.
-    HODpar is a list of HOD parameters in the following order:
+
+def populate(halo_mass, hod_par):
+
+    """
+    halo_mass is a numpy array with halo masses.
+    hod_par is a list of HOD parameters in the following order:
     (log10(Mcut), log10(M1), sigma, kappa, alpha)
-    
-    Returns two arrays one for centrals one for satellites. 
+
+    Returns two arrays one for centrals one for satellites.
     The centrals array has 1 (there is a central) or 0 (no central).
     The satellites array has the number of satellites.
-    '''
+    """
 
-    Mcut, M1, sigma, kappa, alpha = HODpar
+    Mcut, M1, sigma, kappa, alpha = hod_par
     # Central galaxies
-    NcenProb = 0.5*scipy.special.erfc((Mcut - np.log10(Mhalos))/np.sqrt(2)/sigma)
+    NcenProb: float = 0.5*scipy.special.erfc((Mcut - np.log10(halo_mass)) / np.sqrt(2) / sigma)
     Ncen = np.random.poisson(NcenProb)
     # Can only have one central galaxy. Turn Ncen > 1 to 1
     Ncen[np.where(Ncen > 0)] = 1
 
     # Satellite galaxies
-    NsatProb = NcenProb*((Mhalos - kappa*10**Mcut)/10**M1)**alpha
+    NsatProb = NcenProb * ((halo_mass - kappa * 10 ** Mcut) / 10 ** M1) ** alpha
     NsatProb[np.where(np.isnan(NsatProb))] = 0
     NsatProb[np.where(NsatProb < 1)] = 0
     Nsat = np.random.poisson(NsatProb)
 
     return Ncen, Nsat
 
-def place_satellites(halos, Nsat):
-    '''
+
+def place_satellites(halos, n_sat):
+    """
     halos - halo mass, virial radius, x, y, z
     Nsat - Number of satellites I need to place
     Both of the above are arrays.
 
     returns x, y, z coordinates of satellite galaxies assuming NFW profile.
-    '''
+    """
     # Rs is in the second column and it is in kpc so convert to Mpc
     M, Rs, xh, yh, zh = halos.T
     Rs /= 1000.0
     # Total number of satellites
-    TotSat = np.sum(Nsat)
+    tot_sat = int(np.sum(n_sat))
     # Total number of halos
-    Nhalo = np.size(Rs)
+    nhalo = np.size(Rs)
 
     # Random direction
-    x = np.random.randn(TotSat)
-    y = np.random.randn(TotSat)
-    z = np.random.randn(TotSat)
+    x = np.random.randn(tot_sat)
+    y = np.random.randn(tot_sat)
+    z = np.random.randn(tot_sat)
     # Make it unit length
     norm = np.sqrt(x**2 + y**2 + z**2)
     x /= norm
     y /= norm
     z /= norm
-    distance = np.zeros(TotSat)
+    distance = np.zeros(tot_sat)
     # Concentration parameter is usually not much larger than 10
-    eta = np.random.uniform(0, 2, TotSat)
-    for i in range(TotSat):
-        func = lambda x: np.log(1+x) + 1.0/(1+x) - 1 - eta[i]
-        rstar = scipy.optimize.fsolve(func, 1)
-        distance[i] = Rs[i]**3*rstar
+    eta = np.random.uniform(0, 2, tot_sat)
+    for i in range(tot_sat):
+        def func(r):
+            np.log(1 + r) + 1.0/(1 + r) - 1 - eta[i]
+
+        rstar = scipy.optimize.fsolve(func, np.ndarray([1]))
+        distance[i] = Rs[i]*rstar
     dx = np.transpose(x*distance)
     dy = np.transpose(y*distance)
     dz = np.transpose(z*distance)
     # Now pull all x, y, z of satellites
-    xsat, ysat, zsat = np.zeros((3, TotSat))
+    xsat, ysat, zsat = np.zeros((3, tot_sat))
     counter = 0
-    for i in range(Nhalo):
-        for j in range(Nsat[i]):
+    for i in range(nhalo):
+        for j in range(n_sat[i]):
             xsat[counter] = xh[i] + dx[counter]
             ysat[counter] = yh[i] + dy[counter]
             zsat[counter] = zh[i] + dz[counter]
@@ -83,27 +88,30 @@ def place_satellites(halos, Nsat):
     return xsat, ysat, zsat 
 
 
-def populateHOD(halos, HODpar):
-    '''
-    Take halos and populate them with galaxies according to HODpar
+def populate_hod(halos, hod_par):
+    """
+    Take halos and populate them with galaxies according to hod_par
     parameters.
     halo array must be a numpy array with 5 columns:
     halomass (Mpc/h), Rs (kpc/h), x, y, z (all Mpc/h comoving)
     returns x, y, z of galaxies.
-    '''
-    Mhalos = halos[:,0]
-    Ncen, Nsat = populate(Mhalos, HODpar)
+    """
+    Mhalos = halos[:, 0]
+    Ncen, Nsat = populate(Mhalos, hod_par)
     xcen, ycen, zcen = np.transpose(halos[Ncen, 2:])
     xsat, ysat, zsat = place_satellites(halos, Nsat)
     xyzall = np.hstack(([xcen, ycen, zcen], [xsat, ysat, zsat]))
     return xyzall
 
+
+"""
 if __name__ == '__main__':
     start_time = time.time()
     halos = np.load('/mnt/data1/MDhalos.npy')
     print(time.time() - start_time)
     print(halos)
     HODpar = (13.08, 14.06, 0.98, 1.13, 0.9)
-    xyzall = populateHOD(halos, HODpar)
+    xyzall = populate_hod(halos, HODpar)
     print(xyzall)
     print(time.time() - start_time)
+"""
